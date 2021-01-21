7097caecac45d2d8cd4032b0046608174cbe430e
Becky <becky.gagnon92@gmail.com>
2021-01-20 20:50:50 -0800

    Two tests for the front end complete- calendar render and next month button query

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App.jsx';
+import {render, fireEvent, screen} from '@testing-library/react';
+import { TestWatcher } from 'jest';
 
 it('renders correctly', () => {
   const tree = renderer
@@ -8,3 +10,20 @@ it('renders correctly', () => {
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('Scroll to next month button', async() => {
+  render(<App />)
+  const marchMonthsBefore = await screen.queryByText('March');
+  expect(marchMonthsBefore).toBe(null);
+
+  fireEvent.click(screen.getByText('Check Availability'))
+  fireEvent.click(screen.getByText('>>'));
+
+  const marchMonthsAfter = await screen.findAllByText('March');
+  console.log(marchMonthsAfter)
+
+
+  expect(marchMonthsAfter).toHaveLength(1);
+  //expect(items).toHaveLength(1)
+})
+
