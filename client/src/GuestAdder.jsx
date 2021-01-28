8c0f021760d54d4c53351bbae595ced3ea1eb33c
Becky <becky.gagnon92@gmail.com>
2021-01-28 09:29:39 -0800

    Re-styled the ant design icons

--- client/src/GuestAdder.jsx
+++ client/src/GuestAdder.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
+import {PlusCircleOutlined, MinusCircleOutlined} from '@ant-design/icons';
+
 
 const GuestPickerTable = styled.table`
 
@@ -18,6 +20,12 @@ const PlusCircle = styled.div`
   z-index: 100;
 `;
 
+const CircleIcon = styled.div`
+  font-size: 30px;
+  color: lightgrey;
+
+`;
+
 class GuestAdder extends React.Component {
   constructor(props) {
     super(props);
@@ -33,21 +41,21 @@ class GuestAdder extends React.Component {
           <tbody>
             <tr>
               <GptTD>Adults</GptTD>
-              <td onClick={ () => { this.props.updateGuests({numAdults: this.props.guests.numAdults - 1}); }}><PlusCircle>-</PlusCircle></td>
+              <td onClick={ () => { this.props.updateGuests({numAdults: this.props.guests.numAdults - 1}); }}><CircleIcon><MinusCircleOutlined/></CircleIcon></td>
               <td>{this.props.guests.numAdults}</td>
-              <td onClick={ () => { this.props.updateGuests({numAdults: this.props.guests.numAdults + 1}); }}><PlusCircle>+</PlusCircle></td>
+              <td onClick={ () => { this.props.updateGuests({numAdults: this.props.guests.numAdults + 1}); }}><CircleIcon><PlusCircleOutlined/></CircleIcon></td>
             </tr>
             <tr>
               <GptTD>Children</GptTD>
-              <td onClick={ () => { this.props.updateGuests({numChildren: this.props.guests.numChildren - 1}); }}><PlusCircle>-</PlusCircle></td>
+              <td onClick={ () => { this.props.updateGuests({numChildren: this.props.guests.numChildren - 1}); }}><CircleIcon><MinusCircleOutlined/></CircleIcon></td>
               <td>{this.props.guests.numChildren}</td>
-              <td onClick={ () => { this.props.updateGuests({numChildren: this.props.guests.numChildren + 1}); }}><PlusCircle>+</PlusCircle></td>
+              <td onClick={ () => { this.props.updateGuests({numChildren: this.props.guests.numChildren + 1}); }}><CircleIcon><PlusCircleOutlined/></CircleIcon></td>
             </tr>
             <tr>
               <GptTD>Infants</GptTD>
-              <td onClick={ () => { this.props.updateGuests({numInfants: this.props.guests.numInfants - 1}); }}><PlusCircle>-</PlusCircle></td>
+              <td onClick={ () => { this.props.updateGuests({numInfants: this.props.guests.numInfants - 1}); }}><CircleIcon><MinusCircleOutlined/></CircleIcon></td>
               <td>{this.props.guests.numInfants}</td>
-              <td onClick={ () => { this.props.updateGuests({numInfants: this.props.guests.numInfants + 1}); }}><PlusCircle>+</PlusCircle></td>
+              <td onClick={ () => { this.props.updateGuests({numInfants: this.props.guests.numInfants + 1}); }}><CircleIcon><PlusCircleOutlined/></CircleIcon></td>
             </tr>
           </tbody>
         </GuestPickerTable>
